59d34a9a98a6850e4bfde0becb01b1ff57c5181a
somu3341 <somashekarvsv1996@gmail.com>
2023-03-14 16:44:47 +0530

    [somu]Add:Printing New Contacts.

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.Loader;
+
 namespace AddressBookSystem
 {
     class Program
@@ -6,23 +8,9 @@ namespace AddressBookSystem
         public static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to Address Book\r\nProgram in\r\nAddressBookMain class");
-            CreateContacts createContacts = new CreateContacts();
-            Console.WriteLine("Enter First Name");
-            createContacts.FirstName = Console.ReadLine();
-            Console.WriteLine("Enetr Last Name");
-            createContacts.LastName = Console.ReadLine();
-            Console.WriteLine("Enter Address");
-            createContacts.Address = Console.ReadLine();
-            Console.WriteLine("Enter City");
-            createContacts.City = Console.ReadLine();
-            Console.WriteLine("Enter State");
-            createContacts.Satate = Console.ReadLine();
-            Console.WriteLine("Enter ZIP");
-            createContacts.ZIP = Console.ReadLine();
-            Console.WriteLine("Enetr Phone NUmber");
-            createContacts.PhoneNumber = Console.ReadLine();
-            Console.WriteLine("Enter Email");
-            createContacts.Email = Console.ReadLine();
+            AddressBookMain addressBookMain = new AddressBookMain();
+            addressBookMain.addcontact();
+            addressBookMain.Display(); 
         }
     }
 }
