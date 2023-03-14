1535312a77d264e4ae5301991a5294d7a42b8f95
somu3341 <somashekarvsv1996@gmail.com>
2023-03-14 14:11:50 +0530

    [somu]Add:Craeting Contact Details to Address Book.

---
+++ AddressBookSystem/CreateContacts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class CreateContacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Satate { get; set; }
+        public string ZIP { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace AddressBookSystem
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Address Book\r\nProgram in\r\nAddressBookMain class");
+            CreateContacts createContacts = new CreateContacts();
+            Console.WriteLine("Enter First Name");
+            createContacts.FirstName = Console.ReadLine();
+            Console.WriteLine("Enetr Last Name");
+            createContacts.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            createContacts.Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            createContacts.City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            createContacts.Satate = Console.ReadLine();
+            Console.WriteLine("Enter ZIP");
+            createContacts.ZIP = Console.ReadLine();
+            Console.WriteLine("Enetr Phone NUmber");
+            createContacts.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Email");
+            createContacts.Email = Console.ReadLine();
+        }
+    }
+}
