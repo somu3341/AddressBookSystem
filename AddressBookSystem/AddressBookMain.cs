c156345483f471b331065759c15dc5b425d3b1e8
somu3341 <somashekarvsv1996@gmail.com>
2023-03-15 20:20:28 +0530

    [somu]Add:Printing New Contacts.

---
+++ AddressBookSystem/AddressBookMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+   public class AddressBookMain
+    {
+        CreateContacts Contacts = new CreateContacts();
+        public void addcontact()
+        {
+            Console.WriteLine("Enter First Name");
+            Contacts.FirstName = Console.ReadLine();
+            Console.WriteLine("Enetr Last Name");
+            Contacts.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            Contacts.Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            Contacts.City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            Contacts.State = Console.ReadLine();
+            Console.WriteLine("Enter ZIP");
+            Contacts.ZIP = Console.ReadLine();
+            Console.WriteLine("Enetr Phone NUmber");
+            Contacts.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Email");
+            Contacts.Email = Console.ReadLine();
+        }
+        public void Display()
+        {
+            Console.WriteLine("First name: " + Contacts.FirstName + "\nLastname: " + Contacts.LastName + "\nAddress: " + Contacts.Address +
+                "\nCity: " + Contacts.City + "\nState: " + Contacts.State + "\nZIP: " + Contacts.ZIP + "\nPhone Number: " + Contacts.PhoneNumber + "\nEmail: " + Contacts.Email);
+        }
+    }
+}
--- AddressBookSystem/CreateContacts.cs
+++ AddressBookSystem/CreateContacts.cs
@@ -13,7 +13,7 @@ namespace AddressBookSystem
         public string LastName { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
-        public string Satate { get; set; }
+        public string State { get; set; }
         public string ZIP { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
